91ddae4fbf44a27e32f60be3ccae130e0884898d
Herik Colares Rezende <dev.herik@gmail.com>
2025-08-15 13:06:02 -0300

    feat: expand main function with additional steps and initialize empty modules

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     print("Hello from core-ai-application!")
 
+    # 1. Start the database
+    # 2. Start Celeron and Redis
+    # 3. Get knowledge from the database
+    # 4. Create a knowledge graph
+    # 5. Create a Agno Agent
+    # 6. Start agent-user interaction
+
 
 if __name__ == "__main__":
     main()
